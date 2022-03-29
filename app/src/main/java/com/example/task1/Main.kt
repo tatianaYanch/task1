86d559b5fc6600248e89a69a9ecb721f7fef835f
Tatiana Yanchenko <tatiana.559@mail.ru>
2022-03-29 14:03:13 +0300

    version1

---
+++ app/src/main/java/com/example/task1/Main.kt
@@ -0,0 +1,36 @@
+package com.example.task1
+
+
+fun main() {
+    while (true) {
+        println("Введите первое число:")
+        val firstNumber = readLine()?.toDoubleOrNull()
+        if (firstNumber == null) {
+            println("Ошибка! Вы ввели не число")
+            continue
+        } else {
+            println("Введите второе число:")
+            val secondNumber = readLine()?.toDoubleOrNull()
+            if (secondNumber == null) {
+                println("Ошибка! Вы ввели не число")
+                continue
+            }
+            println("Введите оператор (+, -, *, /):")
+            val operator = readLine()
+            val result = calculate(firstNumber, secondNumber, operator)
+            println(String.format("$firstNumber $operator $secondNumber = %.1f", result))
+        }
+    }
+}
+
+fun calculate(firsNumber: Double, secondNumber: Double, operator: String?): Double {
+    var res = 0.0
+    when (operator) {
+        "+" -> res = firsNumber + secondNumber
+        "-" -> res = firsNumber - secondNumber
+        "*" -> res = firsNumber * secondNumber
+        "/" -> res = firsNumber / secondNumber
+        else -> println("!!!Вы ввели неверный оператор!!!")
+    }
+    return res
+}
